2689a97822f1df6772e27374d4d4d85c1cd5f2bf
VictorHAlbu <vh12albuquerque@gmail.com>
2022-02-06 16:52:23 -0300

    functions TypeScript

---
+++ tiny-functions.ts
@@ -0,0 +1,40 @@
+// BAAD
+async function createPixTransfer(sender:User, receiver: User, amount: number) {
+
+  if(sender.balance < amount){
+    throw new Error(
+      " Sender does not have enough balance to make this transfer"
+      );
+    
+  }
+
+  sender.balance = sender.balance - amount
+  await sender.save();
+
+  receiver.balance = sender.balance + amount
+  await receiver.save();
+  await receiver.notifyNewPix(amount);
+  
+}
+
+// GOOD
+async function createPixTransfer(sender:User, receiver: User, amount: number) {
+  await chargeAmountFromSender(sender, amount);
+  await incrementAmountForReceiver(receiver, amount);
+  await receiver.notifyNewPix(amount);
+}
+
+async function chargeAmountFromSender(sender: User, amount: number ) {
+  if(sender.balance < amount){
+    throw new Error(
+      " Sender does not have enough balance to make this transfer"
+      );
+  }
+  sender.balance = sender.balance - amount
+  await sender.save();
+}
+
+async function incrementAmountForReceiver(receiver: User, amount: number) {
+  receiver.balance = receiver.balance + amount
+  await receiver.save();
+}
